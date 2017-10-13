79083283fc7be4fa89a40f94bcab6037b07ecd3c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-13 12:51:46 -0700

    Parts 2 and 3 done

--- melons.py
+++ melons.py
@@ -1,5 +1,6 @@
 """Classes for melon orders."""
 
+
 class AbstractMelonOrder(object):
     """Any melon order from anywhere"""
 
@@ -16,6 +17,10 @@ class AbstractMelonOrder(object):
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species.lower() == "christmas melon":
+            base_price = 1.5 * base_price
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -47,3 +52,34 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+        """Adds $3 to orders with a qty less than 10."""
+
+        int_total = super(InternationalMelonOrder, self).get_total()
+
+        if self.qty < 10:
+            int_total += 3
+
+        return int_total
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """Melon order for government"""
+
+    passed_inspection = False
+
+    def __init__(self, species, qty):
+        """Initialize melon order for gov"""
+        super(GovernmentMelonOrder, self).__init__(species, qty,
+                                                   0.0, "government")
+
+        # self.passed = False  #talk about in code review
+
+    def mark_inspection(self, passed):
+        """Record fact that inspection has been passed"""
+        #making sure that passed is a bool
+        if isinstance(passed, bool):
+            self.passed_inspection = passed
+        else:
+            print "That is not a valid input"
